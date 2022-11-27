36298d19b7526f37086e9f77a702c426d6fd67e6
Artem Kozlov <109523742+ThisNarciss@users.noreply.github.com>
2022-11-27 22:58:15 +0300

    create gallery with simplelightbox library

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('div.gallery');
+
+const createGalleryEl = array => {
+  const imagesEl = array
+    .map(
+      item => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join('');
+
+  return (list.innerHTML = imagesEl);
+};
+
+createGalleryEl(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
